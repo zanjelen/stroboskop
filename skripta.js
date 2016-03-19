aaf4cc810ad900090656faedc9be807ef20e515c
Zan Jelen <zan.jelen@icloud.com>
2016-03-19 23:34:27 +0000

    Dinamika gumba za zagon stroboskopa

--- skripta.js
+++ skripta.js
@@ -56,8 +56,8 @@ window.addEventListener('load', function() {
 			vrednosti.push(barva.innerHTML);
 		}
 		
-		minCas = 1000;
-		maxCas = 1000;
+		minCas = parseInt(document.querySelector("#min").value);
+		maxCas = parseInt(document.querySelector("#max").value);
 		spremeniBarvo(0);
 		
 		var start = document.querySelector("#start");
